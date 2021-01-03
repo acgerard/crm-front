81ead93ee2b3e3f5fda26862ea6e9c2a4eaf068d
acgerard <ac.gerard@gmail.com>
2021-01-03 19:00:44 +0100

    Virtualized table

--- src/components/client/client-list.jsx
+++ src/components/client/client-list.jsx
@@ -1,17 +1,10 @@
-import React, {useState} from "react";
-import TableContainer from "@material-ui/core/TableContainer";
+import React, {useEffect, useState} from "react";
 import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import TableCell from "@material-ui/core/TableCell";
-import TableBody from "@material-ui/core/TableBody";
-import TableSortLabel from "@material-ui/core/TableSortLabel";
 import {useDispatch, useSelector} from "react-redux";
 import {getClients, getFilterClient, getSelectedClientId} from "../../selectors/client-selectors";
 import {selectClient} from "../../reducer/client-reducer";
-import CheckIcon from '@material-ui/icons/Check';
-import ClearIcon from '@material-ui/icons/Clear';
+import {VirtualizedTable} from "../virtualized-table";
+import "./client-list.css"
 
 export function ConnectedClientList() {
     const dispatch = useDispatch();
@@ -29,26 +22,17 @@ export function ConnectedClientList() {
     );
 }
 
-export function ClientList({clients, filterClient, selectedClientId, handleSelectClient}) {
-    const headColumns = [
-        {id: "name.first", label: "First Name"},
-        {id: "name.last", label: "Last Name"},
-        {id: "active", label: "Active"},
-        {id: "company", label: "Company"},
-        {id: "dtcf_contact", label: "DTCF Contact"},
-        {id: "country", label: "Country"},
-        {id: "comment", label: "Comment"},
-    ];
+const headColumns = [
+    {dataKey: "firstName", label: "First Name", width: 200},
+    {dataKey: "lastName", label: "Last Name", width: 200},
+    {dataKey: "active", label: "Active", width: 100, boolean: true},
+    {dataKey: "company", label: "Company", width: 200},
+    {dataKey: "dtcf_contact", label: "DTCF Contact", width: 200},
+    {dataKey: "country", label: "Country", width: 150},
+    {dataKey: "comment", label: "Comment", width: 300},
+];
 
-    const [orderBy, setOrderBy] = useState(headColumns[0].id);
-    const [order, setOrder] = useState('asc');
-
-
-    const handleRequestSort = property => () => {
-        const isAsc = orderBy === property && order === 'asc';
-        setOrder(isAsc ? 'desc' : 'asc');
-        setOrderBy(property);
-    };
+export function ClientList({clients, filterClient, handleSelectClient}) {
 
     function filteredClients() {
         if (filterClient !== '') {
@@ -56,89 +40,35 @@ export function ClientList({clients, filterClient, selectedClientId, handleSelec
             return clients.filter(client => {
                 return regex.test(client.name.first) ||
                     regex.test(client.name.last) ||
-                    regex.test(client.name.email)
+                    regex.test(client.country) ||
+                    regex.test(client.comment) ||
+                    regex.test(client.company) ||
+                    regex.test(client.dtcf_contact)
             });
         } else return clients
     }
 
+    const data = filteredClients().map(client => {
+        return {
+            _id: client._id,
+            firstName: client.name.first,
+            lastName: client.name.last,
+            country: client.addresses?.pro?.country || client.addresses?.perso?.country,
+            dtcf_contact: client.dtcf_contact,
+            comment: client.comment,
+            company: client.company,
+            active: client.active
+        }
+    });
 
     return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        {headColumns.map(column => {
-                            return <TableCell
-                                sortDirection={orderBy === column.id ? order : false}
-                                key={column.id}
-                            >
-                                <TableSortLabel
-                                    active={orderBy === column.id}
-                                    direction={orderBy === column.id ? order : 'asc'}
-                                    onClick={handleRequestSort(column.id)}
-                                >
-                                    {column.label}
-                                </TableSortLabel>
-                            </TableCell>
-                        })}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {stableSort(filteredClients(), getComparator(order, orderBy)).map(client => {
-                        const isSelected = client._id === selectedClientId;
-                        return (
-                            <TableRow
-                                key={client._id}
-                                hover
-                                onClick={() => handleSelectClient(client._id)}
-                                selected={isSelected}
-                            >
-                                <TableCell component="th" scope="row">{client.name.first}</TableCell>
-                                <TableCell>{client.name.last}</TableCell>
-                                <TableCell>{client.active ? <CheckIcon/> : <ClearIcon/>}</TableCell>
-                                <TableCell>{client.company}</TableCell>
-                                <TableCell>{client.dtcf_contact}</TableCell>
-                                <TableCell>{client.country}</TableCell>
-                                <TableCell>{client.comment}</TableCell>
-                            </TableRow>
-                        );
-                    })}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <Paper className="client-list">
+            <VirtualizedTable
+                rowCount={data.length}
+                data={data}
+                columns={headColumns}
+                onRowClick={({index}) => handleSelectClient(data[index]._id)}
+            />
+        </Paper>
     );
-}
-
-
-function descendingComparator(a, b, orderBy) {
-    const properties = orderBy.split('.');
-    let valueA = a;
-    let valueB = b;
-    properties.forEach(prop => {
-        valueA = valueA[prop];
-        valueB = valueB[prop];
-    });
-    if (valueB < valueA) {
-        return -1;
-    }
-    if (valueB > valueA) {
-        return 1;
-    }
-    return 0;
-}
-
-function getComparator(order, orderBy) {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) return order;
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
 }
--- src/components/client/client-list.stories.js
+++ src/components/client/client-list.stories.js
@@ -21,7 +21,7 @@ WithClients.args = {
             active: true,
             company: "Stark Enterprises",
             dtcf_contact: "Nick Fury",
-            country: "USA",
+            addresses: {pro:{country:"USA"}},
             comment: "Magnificent comment"
         },
         {
@@ -30,7 +30,7 @@ WithClients.args = {
             active: true,
             company: "Stark Enterprises",
             dtcf_contact: "Nick Fury",
-            country: "USA",
+            addresses: {pro:{country:"USA"}},
             comment: "Magnificent comment"
         },
     ]
@@ -47,11 +47,27 @@ WithLongComment.args = {
         {
             _id: 1,
             name: {first: "Tony", last: "Stark"},
-            active: true,
+            active: false,
             company: "Stark Enterprises",
             dtcf_contact: "Nick Fury",
-            country: "USA",
+            addresses: {pro:{country:"USA"}},
             comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non nunc sed elit consequat placerat sed bibendum lectus. Nulla diam magna, eleifend ac libero sed, fermentum convallis est. Aliquam sollicitudin sem eu lorem ullamcorper porttitor. Fusce sit amet lorem urna. In nulla magna, tempus vitae commodo quis, ullamcorper vitae magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce maximus interdum ante ac congue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus efficitur erat id sapien bibendum, sed vestibulum massa commodo. Integer sit amet metus eget libero luctus lacinia. Curabitur in odio vitae ligula dignissim rutrum eu non erat. Aliquam rhoncus pretium varius. Morbi semper augue quis purus faucibus vehicula. Donec quis elit a urna hendrerit tempor. Suspendisse orci velit, interdum euismod rhoncus eu, facilisis nec dolor. "
         }
     ]
+};
+
+export const Huge = Template.bind({});
+Huge.args = {
+    clients: [...Array(1000).keys()].map(index => {
+            return {
+                _id: index,
+                name: {first: `FirstName${index}`, last: `LastName${index}`},
+                active: true,
+                company: "Company",
+                dtcf_contact: "Contact",
+                addresses: {pro:{country:"France"}},
+                comment: "Great comment"
+            };
+        }
+    )
 };
