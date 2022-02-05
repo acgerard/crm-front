55ecb552ac7ff82d0222f96efdb5252219452c76
agerard <ac.gerard@gmail.com>
2022-02-05 01:17:41 +0100

    Update image in SignIn

--- src/components/SignInSide.jsx
+++ src/components/SignInSide.jsx
@@ -16,7 +16,7 @@ const useStyles = makeStyles(theme => ({
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundImage: 'url(fond-cerisiers.jpg)',
     backgroundRepeat: 'no-repeat',
     backgroundColor: theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
     backgroundSize: 'cover',
